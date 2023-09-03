a9270127a6ec2532c699b3bf368ec495b0c63d5c
Eric Ovenden <eric2ovenden@gmail.com>
2023-09-02 23:52:31 -0500

    Changed "use" dependency structure.
    Moved common use imports from private modules to the header of the
    file.
    
    Additionally, removed the private modules. These were just to
    seperate specific #![cfg] macros. Instead, this structure was
    replaced with the "cfg_if!" macro from the crate `cfg_if`.

--- src/vectors/mod.rs
+++ src/vectors/mod.rs
@@ -1,3 +1,5 @@
+use adv_linalg_proc_macro::{impl_dot_product, impl_vector_add, impl_vector_sub};
+use core::ops::{Add, Mul, Sub};
 use cfg_if::cfg_if;
 
 cfg_if! {
@@ -32,165 +34,106 @@ cfg_if! {
     }
 }
 
-// implement addition
+cfg_if! {
+    if #[cfg(feature = "full")] {
+        impl_vector_add!(
+            impl<T: Clone + Add<Output = T>> Add for [Vector<T>] + [Vector<T>];
+            impl<'lhs, T: Clone + Add<Output = T>> Add for [VectorSlice<'lhs, T>] + [Vector<T>];
+            impl<'rhs, T: Clone + Add<Output = T>> Add for [Vector<T>] + [VectorSlice<'rhs, T>];
+            impl<'lhs, 'rhs, T: Clone + Add<Output = T>> Add for [VectorSlice<'lhs, T>] + [VectorSlice<'rhs, T>];
 
-mod no_std_only {
-    #![cfg(all(feature = "no_std", not(feature = "full")))]
+            #[mut_left] impl<T: Clone + Add<Output = T>> Add for [MutVector<T>] + [Vector<T>];
+            #[mut_left] impl<'lhs, T: Clone + Add<Output = T>> Add for [MutVectorSlice<'lhs, T>] + [Vector<T>];
+            #[mut_left] impl<'rhs, T: Clone + Add<Output = T>> Add for [MutVector<T>] + [VectorSlice<'rhs, T>];
+            #[mut_left] impl<'lhs, 'rhs, T: Clone + Add<Output = T>> Add for [MutVectorSlice<'lhs, T>] + [VectorSlice<'rhs, T>];
 
-    use super::*;
-    use adv_linalg_proc_macro::{impl_dot_product, impl_vector_add, impl_vector_sub};
-    use core::ops::{Add, Mul, Sub};
+            #[mut_right] impl<T: Clone + Add<Output = T>> Add for [Vector<T>] + [MutVector<T>];
+            #[mut_right] impl<'lhs, T: Clone + Add<Output = T>> Add for [VectorSlice<'lhs, T>] + [MutVector<T>];
+            #[mut_right] impl<'rhs, T: Clone + Add<Output = T>> Add for [Vector<T>] + [MutVectorSlice<'rhs, T>];
+            #[mut_right] impl<'lhs, 'rhs, T: Clone + Add<Output = T>> Add for [VectorSlice<'lhs, T>] + [MutVectorSlice<'rhs, T>];
 
-    impl_vector_add!(
-        #[no_std] #[mut_left]
-        impl<'lhs, 'rhs, T: Clone + Add<Output = T>>
-            Add for
-                [MutVectorSlice<'lhs, T>] + [VectorSlice<'rhs, T>];
+            #[mut_both] impl<T: Clone + Add<Output = T>> Add for [MutVector<T>] + [MutVector<T>];
+            #[mut_both] impl<'lhs, T: Clone + Add<Output = T>> Add for [MutVectorSlice<'lhs, T>] + [MutVector<T>];
+            #[mut_both] impl<'rhs, T: Clone + Add<Output = T>> Add for [MutVector<T>] + [MutVectorSlice<'rhs, T>];
+            #[mut_both] impl<'lhs, 'rhs, T: Clone + Add<Output = T>> Add for [MutVectorSlice<'lhs, T>] + [MutVectorSlice<'rhs, T>]
+        );
 
-        #[no_std] #[mut_both]
-        impl<'lhs, 'rhs, T: Clone + Add<Output = T>>
-            Add for
-                [MutVectorSlice<'lhs, T>] + [MutVectorSlice<'rhs, T>]
-    );
+        impl_dot_product!(
+            impl<T: Clone + Default + Add<Output = T> + Mul<Output = T>> Mul for [Vector<T>] * [Vector<T>];
+            impl<'lhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>> Mul for [VectorSlice<'lhs, T>] * [Vector<T>];
+            impl<'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>> Mul for [Vector<T>] * [VectorSlice<'rhs, T>];
+            impl<'lhs, 'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>> Mul for [VectorSlice<'lhs, T>] * [VectorSlice<'rhs, T>];
 
-    impl_dot_product!(
-        #[mut_left]
-        impl<'lhs, 'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for
-                [MutVectorSlice<'lhs, T>] * [VectorSlice<'rhs, T>];
+            #[mut_left] impl<T: Clone + Default + Add<Output = T> + Mul<Output = T>> Mul for [MutVector<T>] * [Vector<T>];
+            #[mut_left] impl<'lhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>> Mul for [MutVectorSlice<'lhs, T>] * [Vector<T>];
+            #[mut_left] impl<'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>> Mul for [MutVector<T>] * [VectorSlice<'rhs, T>];
+            #[mut_left] impl<'lhs, 'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>> Mul for [MutVectorSlice<'lhs, T>] * [VectorSlice<'rhs, T>];
 
-        #[mut_both]
-        impl<'lhs, 'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for
-                [MutVectorSlice<'lhs, T>] * [MutVectorSlice<'rhs, T>]
-    );
+            #[mut_right] impl<T: Clone + Default + Add<Output = T> + Mul<Output = T>> Mul for [Vector<T>] * [MutVector<T>];
+            #[mut_right] impl<'lhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>> Mul for [VectorSlice<'lhs, T>] * [MutVector<T>];
+            #[mut_right] impl<'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>> Mul for [Vector<T>] * [MutVectorSlice<'rhs, T>];
+            #[mut_right] impl<'lhs, 'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>> Mul for [VectorSlice<'lhs, T>] * [MutVectorSlice<'rhs, T>];
 
-    impl_vector_sub!(
-        #[no_std] #[mut_left]
-        impl<'lhs, 'rhs, T: Clone + Sub<Output = T>>
-            Sub for
-                [MutVectorSlice<'lhs, T>] - [VectorSlice<'rhs, T>];
+            #[mut_both] impl<T: Clone + Default + Add<Output = T> + Mul<Output = T>> Mul for [MutVector<T>] * [MutVector<T>];
+            #[mut_both] impl<'lhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>> Mul for [MutVectorSlice<'lhs, T>] * [MutVector<T>];
+            #[mut_both] impl<'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>> Mul for [MutVector<T>] * [MutVectorSlice<'rhs, T>];
+            #[mut_both] impl<'lhs, 'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>> Mul for [MutVectorSlice<'lhs, T>] * [MutVectorSlice<'rhs, T>]
+        );
 
-        #[no_std] #[mut_both]
-        impl<'lhs, 'rhs, T: Clone + Sub<Output = T>>
-            Sub for
-                [MutVectorSlice<'lhs, T>] - [MutVectorSlice<'rhs, T>]
-    );
-}
+        impl_vector_sub!(
+            impl<T: Clone + Sub<Output = T>> Sub for [Vector<T>] - [Vector<T>];
+            impl<'lhs, T: Clone + Sub<Output = T>> Sub for [VectorSlice<'lhs, T>] - [Vector<T>];
+            impl<'rhs, T: Clone + Sub<Output = T>> Sub for [Vector<T>] - [VectorSlice<'rhs, T>];
+            impl<'lhs, 'rhs, T: Clone + Sub<Output = T>> Sub for [VectorSlice<'lhs, T>] - [VectorSlice<'rhs, T>];
 
-mod full_only {
-    #![cfg(feature = "full")]
+            #[mut_left] impl<T: Clone + Sub<Output = T>> Sub for [MutVector<T>] - [Vector<T>];
+            #[mut_left] impl<'lhs, T: Clone + Sub<Output = T>> Sub for [MutVectorSlice<'lhs, T>] - [Vector<T>];
+            #[mut_left] impl<'rhs, T: Clone + Sub<Output = T>> Sub for [MutVector<T>] - [VectorSlice<'rhs, T>];
+            #[mut_left] impl<'lhs, 'rhs, T: Clone + Sub<Output = T>> Sub for [MutVectorSlice<'lhs, T>] - [VectorSlice<'rhs, T>];
 
-    use super::*;
-    use adv_linalg_proc_macro::{impl_dot_product, impl_vector_add, impl_vector_sub};
-    use core::ops::{Add, Mul, Sub};
+            #[mut_right] impl<T: Clone + Sub<Output = T>> Sub for [Vector<T>] - [MutVector<T>];
+            #[mut_right] impl<'lhs, T: Clone + Sub<Output = T>> Sub for [VectorSlice<'lhs, T>] - [MutVector<T>];
+            #[mut_right] impl<'rhs, T: Clone + Sub<Output = T>> Sub for [Vector<T>] - [MutVectorSlice<'rhs, T>];
+            #[mut_right] impl<'lhs, 'rhs, T: Clone + Sub<Output = T>> Sub for [VectorSlice<'lhs, T>] - [MutVectorSlice<'rhs, T>];
 
-    impl_vector_add!(
-        impl<T: Clone + Add<Output = T>> Add for [Vector<T>] + [Vector<T>];
-        impl<'lhs, T: Clone + Add<Output = T>> Add for [VectorSlice<'lhs, T>] + [Vector<T>];
-        impl<'rhs, T: Clone + Add<Output = T>> Add for [Vector<T>] + [VectorSlice<'rhs, T>];
-        impl<'lhs, 'rhs, T: Clone + Add<Output = T>> Add for [VectorSlice<'lhs, T>] + [VectorSlice<'rhs, T>];
-
-        #[mut_left] impl<T: Clone + Add<Output = T>> Add for [MutVector<T>] + [Vector<T>];
-        #[mut_left] impl<'lhs, T: Clone + Add<Output = T>> Add for [MutVectorSlice<'lhs, T>] + [Vector<T>];
-        #[mut_left] impl<'rhs, T: Clone + Add<Output = T>> Add for [MutVector<T>] + [VectorSlice<'rhs, T>];
-        #[mut_left] impl<'lhs, 'rhs, T: Clone + Add<Output = T>> Add for [MutVectorSlice<'lhs, T>] + [VectorSlice<'rhs, T>];
-
-        #[mut_right] impl<T: Clone + Add<Output = T>> Add for [Vector<T>] + [MutVector<T>];
-        #[mut_right] impl<'lhs, T: Clone + Add<Output = T>> Add for [VectorSlice<'lhs, T>] + [MutVector<T>];
-        #[mut_right] impl<'rhs, T: Clone + Add<Output = T>> Add for [Vector<T>] + [MutVectorSlice<'rhs, T>];
-        #[mut_right] impl<'lhs, 'rhs, T: Clone + Add<Output = T>> Add for [VectorSlice<'lhs, T>] + [MutVectorSlice<'rhs, T>];
-
-        #[mut_both] impl<T: Clone + Add<Output = T>> Add for [MutVector<T>] + [MutVector<T>];
-        #[mut_both] impl<'lhs, T: Clone + Add<Output = T>> Add for [MutVectorSlice<'lhs, T>] + [MutVector<T>];
-        #[mut_both] impl<'rhs, T: Clone + Add<Output = T>> Add for [MutVector<T>] + [MutVectorSlice<'rhs, T>];
-        #[mut_both] impl<'lhs, 'rhs, T: Clone + Add<Output = T>> Add for [MutVectorSlice<'lhs, T>] + [MutVectorSlice<'rhs, T>]
-    );
-
-    impl_dot_product!(
-        impl<T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for
-                [Vector<T>] * [Vector<T>];
-        impl<'lhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for
-                [VectorSlice<'lhs, T>] * [Vector<T>];
-        impl<'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for
-                [Vector<T>] * [VectorSlice<'rhs, T>];
-        impl<'lhs, 'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for
-                [VectorSlice<'lhs, T>] * [VectorSlice<'rhs, T>];
-
-        #[mut_left]
-        impl<T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for
-                [MutVector<T>] * [Vector<T>];
-        #[mut_left]
-        impl<'lhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for
-                [MutVectorSlice<'lhs, T>] * [Vector<T>];
-        #[mut_left]
-        impl<'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for
-                [MutVector<T>] * [VectorSlice<'rhs, T>];
-        #[mut_left]
-        impl<'lhs, 'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for
-                [MutVectorSlice<'lhs, T>] * [VectorSlice<'rhs, T>];
-
-        #[mut_right]
-        impl<T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for
-                [Vector<T>] * [MutVector<T>];
-        #[mut_right]
-        impl<'lhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for
-                [VectorSlice<'lhs, T>] * [MutVector<T>];
-        #[mut_right]
-        impl<'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for
-                [Vector<T>] * [MutVectorSlice<'rhs, T>];
-        #[mut_right]
-        impl<'lhs, 'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for
-                [VectorSlice<'lhs, T>] * [MutVectorSlice<'rhs, T>];
-
-        #[mut_both]
-        impl<T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for
-                [MutVector<T>] * [MutVector<T>];
-        #[mut_both]
-        impl<'lhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for
-                [MutVectorSlice<'lhs, T>] * [MutVector<T>];
-        #[mut_both]
-        impl<'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for [MutVector<T>] * [MutVectorSlice<'rhs, T>];
-        #[mut_both]
-        impl<'lhs, 'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>>
-            Mul for
-                [MutVectorSlice<'lhs, T>] * [MutVectorSlice<'rhs, T>]
-    );
-
-    impl_vector_sub!(
-        impl<T: Clone + Sub<Output = T>> Sub for [Vector<T>] - [Vector<T>];
-        impl<'lhs, T: Clone + Sub<Output = T>> Sub for [VectorSlice<'lhs, T>] - [Vector<T>];
-        impl<'rhs, T: Clone + Sub<Output = T>> Sub for [Vector<T>] - [VectorSlice<'rhs, T>];
-        impl<'lhs, 'rhs, T: Clone + Sub<Output = T>> Sub for [VectorSlice<'lhs, T>] - [VectorSlice<'rhs, T>];
-
-        #[mut_left] impl<T: Clone + Sub<Output = T>> Sub for [MutVector<T>] - [Vector<T>];
-        #[mut_left] impl<'lhs, T: Clone + Sub<Output = T>> Sub for [MutVectorSlice<'lhs, T>] - [Vector<T>];
-        #[mut_left] impl<'rhs, T: Clone + Sub<Output = T>> Sub for [MutVector<T>] - [VectorSlice<'rhs, T>];
-        #[mut_left] impl<'lhs, 'rhs, T: Clone + Sub<Output = T>> Sub for [MutVectorSlice<'lhs, T>] - [VectorSlice<'rhs, T>];
-
-        #[mut_right] impl<T: Clone + Sub<Output = T>> Sub for [Vector<T>] - [MutVector<T>];
-        #[mut_right] impl<'lhs, T: Clone + Sub<Output = T>> Sub for [VectorSlice<'lhs, T>] - [MutVector<T>];
-        #[mut_right] impl<'rhs, T: Clone + Sub<Output = T>> Sub for [Vector<T>] - [MutVectorSlice<'rhs, T>];
-        #[mut_right] impl<'lhs, 'rhs, T: Clone + Sub<Output = T>> Sub for [VectorSlice<'lhs, T>] - [MutVectorSlice<'rhs, T>];
-
-        #[mut_both] impl<T: Clone + Sub<Output = T>> Sub for [MutVector<T>] - [MutVector<T>];
-        #[mut_both] impl<'lhs, T: Clone + Sub<Output = T>> Sub for [MutVectorSlice<'lhs, T>] - [MutVector<T>];
-        #[mut_both] impl<'rhs, T: Clone + Sub<Output = T>> Sub for [MutVector<T>] - [MutVectorSlice<'rhs, T>];
-        #[mut_both] impl<'lhs, 'rhs, T: Clone + Sub<Output = T>> Sub for [MutVectorSlice<'lhs, T>] - [MutVectorSlice<'rhs, T>]
-    );
+            #[mut_both] impl<T: Clone + Sub<Output = T>> Sub for [MutVector<T>] - [MutVector<T>];
+            #[mut_both] impl<'lhs, T: Clone + Sub<Output = T>> Sub for [MutVectorSlice<'lhs, T>] - [MutVector<T>];
+            #[mut_both] impl<'rhs, T: Clone + Sub<Output = T>> Sub for [MutVector<T>] - [MutVectorSlice<'rhs, T>];
+            #[mut_both] impl<'lhs, 'rhs, T: Clone + Sub<Output = T>> Sub for [MutVectorSlice<'lhs, T>] - [MutVectorSlice<'rhs, T>]
+        );
+    } else if #[cfg(feature = "no_std")] {
+        impl_vector_add!(
+            #[no_std] #[mut_left]
+            impl<'lhs, 'rhs, T: Clone + Add<Output = T>>
+                Add for
+                    [MutVectorSlice<'lhs, T>] + [VectorSlice<'rhs, T>];
+    
+            #[no_std] #[mut_both]
+            impl<'lhs, 'rhs, T: Clone + Add<Output = T>>
+                Add for
+                    [MutVectorSlice<'lhs, T>] + [MutVectorSlice<'rhs, T>]
+        );
+    
+        impl_dot_product!(
+            #[mut_left]
+            impl<'lhs, 'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>>
+                Mul for
+                    [MutVectorSlice<'lhs, T>] * [VectorSlice<'rhs, T>];
+    
+            #[mut_both]
+            impl<'lhs, 'rhs, T: Clone + Default + Add<Output = T> + Mul<Output = T>>
+                Mul for
+                    [MutVectorSlice<'lhs, T>] * [MutVectorSlice<'rhs, T>]
+        );
+    
+        impl_vector_sub!(
+            #[no_std]
+            #[mut_left]
+            impl<'lhs, 'rhs, T: Clone + Sub<Output = T>> Sub for [MutVectorSlice<'lhs, T>] - [VectorSlice<'rhs, T>];
+    
+            #[no_std]
+            #[mut_both] 
+            impl<'lhs, 'rhs, T: Clone + Sub<Output = T>> Sub for [MutVectorSlice<'lhs, T>] - [MutVectorSlice<'rhs, T>]
+        );
+    }
 }
